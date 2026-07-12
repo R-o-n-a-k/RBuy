5303db783b84ca40c501eb94fea098036afca920
Ronak Patel <rroonnaakk7@gmail.com>
2026-07-13 00:29:03 +0530

    feat: adding multiple heading for shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import SectionHeading from '../components/SectionHeading'
 
 const Shop = () => {
   return (
-    <div className="page">Shop</div>
+    <div className="page">
+
+      <section className="my-10">
+        <SectionHeading
+          title="Men's Clothing"
+          linkText="View All"
+          linkTo="/shop/men"
+        />
+      </section>
+
+      <section className="my-10">
+        <SectionHeading
+          title="Women's Clothing"
+          linkText="View All"
+          linkTo="/shop/women"
+        />
+      </section>
+
+      <section className="my-10">
+        <SectionHeading
+          title="Electronics"
+          linkText="View All"
+          linkTo="/shop/electronics"
+        />
+      </section>
+
+      <section className="my-10">
+        <SectionHeading
+          title="Jewellery"
+          linkText="View All"
+          linkTo="/shop/jewellery"
+        />
+      </section>
+
+    </div>
   )
 }
 
